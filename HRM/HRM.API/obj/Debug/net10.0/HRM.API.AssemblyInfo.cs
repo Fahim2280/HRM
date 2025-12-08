6f05c0cf4e2ab01e45190e83a21a58ebe16e3890
Md. Fahim Alam <kfahim2280@gmail.com>
2025-12-09 02:30:40 +0600

    push

--- HRM/HRM.API/obj/Debug/net10.0/HRM.API.AssemblyInfo.cs
+++ HRM/HRM.API/obj/Debug/net10.0/HRM.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4b1aa2354b00ef47b8910030d8cf83287250f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf9c687f099b57383923087a97f0074a9ca859b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
